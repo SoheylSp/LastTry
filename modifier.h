28aef9d57669b44456ca1d4400e90f15fd8a06ca
SoheylSp <144216711+SoheylSp@users.noreply.github.com>
2024-06-06 17:57:08 +0330

    Add files via upload

---
+++ modifier.h
@@ -0,0 +1,24 @@
+#ifndef MODIFIERCLASS_H
+#define MODIFIERCLASS_H
+
+#include <QJsonObject>
+#include <QGeoCoordinate>
+#include <QGeoPolygon>
+
+
+class ModifierClass
+{
+public:
+    ModifierClass();
+
+    static QJsonObject rotateModifier(const QGeoCoordinate &rotatePoint, double rotateAngle, const QJsonObject &feature);
+
+    static QJsonObject translatorModifier(double translateNumber,QJsonObject feature);
+
+    static QJsonObject scaleModifier(QGeoCoordinate scalePoint, double scaleNumber, QJsonObject feature);
+
+    static QGeoCoordinate findCenterOfGeometry(const QJsonObject& feature);
+
+};
+
+#endif // MODIFIERCLASS_H
